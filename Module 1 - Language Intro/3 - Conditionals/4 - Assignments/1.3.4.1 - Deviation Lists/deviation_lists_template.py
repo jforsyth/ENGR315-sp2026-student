eba90c538884c488decb2aa83196352c31924e44
Josh Jones <jone23jt@dukes.jmu.edu>
2026-02-02 15:02:33 -0500

    2-2-26 deviation lists

--- Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.1 - Deviation Lists/deviation_lists_template.py
+++ Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.1 - Deviation Lists/deviation_lists_template.py
@@ -23,10 +23,13 @@ random_list_B = random.sample(range(100), random_length)
 # use the std() method from numpy to determine which list has the largest standard deviation
 
 ### YOUR CODE HERE
-
+if  np.std(random_list_A) >= np.std(random_list_B):
+    myList = random_list_A
+else:
+    myList = random_list_B
 # set this variable equal to the list with the largest standard deviation
 # do not modify this variable's name, you can/should adjust the contents ;)
 # e.g. longest_list_is = myList
-longest_list_is = None
+longest_list_is = myList
 
 ### YOUR CODE HERE
